3fcaae377f56585f62862545bbab38d5930426fb
damianfadri <damianfadri@gmail.com>
2025-04-02 01:05:51 +0800

    add textures

---
+++ scripts/questions.js
@@ -0,0 +1,62 @@
+export const questions = [
+  {
+      question: "Sino ang mas patient?",
+      left: "Yam",
+      right: "If",
+      answer: "If"
+  },
+  {
+      question: "Ano ang term of endearment nila?",
+      left: "Hon",
+      right: "Dear",
+      answer: "Hon",
+  },
+  {
+      question: "Anong year sila unang naging classmates?",
+      left: "2nd Year HS",
+      right: "3rd Year HS",
+      answer: "2nd Year HS"
+  },
+  {
+      question: "Saan nag-propose si Yam kay If?",
+      left: "San Sebastian",
+      right: "Las Casas",
+      answer: "Las Casas"
+  },
+  {
+      question: "Ano ang 1st gift ni Yam kay If?",
+      left: "Juice",
+      right: "Siomai",
+      answer: "Juice"
+  },
+  {
+      question: "Saan sila unang nag-date?",
+      left: "Mcdo",
+      right: "Jollibee",
+      answer: "Mcdo"
+  },
+  {
+      question: "Sino ang mas masungit?",
+      left: "Yam",
+      right: "If",
+      answer: "Yam"
+  },
+  {
+      question: "Sino ang mas matanda?",
+      left: "Yam",
+      right: "If",
+      answer: "If"
+  },
+  {
+      question: "Sino ang nag-serve sa church nung HS?",
+      left: "Yam",
+      right: "If",
+      answer: "Yam"
+  },
+  {
+      question: "Sino ang mas ma-Lazada?",
+      left: "Yam",
+      right: "If",
+      answer: "If"
+  }
+];
